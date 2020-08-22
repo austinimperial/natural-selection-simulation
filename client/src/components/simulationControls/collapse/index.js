b9e2cda88fe0b92ee41d1aa9b19c385ec6e279e4
Austin Imperial <austin.imperial1@gmail.com>
2020-08-22 16:05:58 -0400

    herokuuu

--- client/src/components/simulationControls/collapse/index.js
+++ client/src/components/simulationControls/collapse/index.js
@@ -7,6 +7,7 @@ function Collapse({children,isCollapsed,collapseContainerStyle,onClick}) {
 
     useEffect(() => {
         setHeight(containerRef.current.scrollHeight)
+        console.log('height',height)
     })
 
     return (
